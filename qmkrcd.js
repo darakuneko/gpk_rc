a12bb99d856c87e8e57a020e0d3ef9ce6f69f367
darakuneko <tvkids@gmail.com>
2024-02-28 16:51:15 +0900

    fix: node-hid open

--- qmkrcd.js
+++ qmkrcd.js
@@ -53,12 +53,12 @@ const getKBDList = () => HID.devices().filter(d =>
     d.usagePage === DEFAULT_USAGE.usagePage
 ).sort((a, b) => `${a.manufacturer}${a.product}` > `${b.manufacturer}${b.product}` ? 1 : -1)
 
-const start = (device) => {
+const start = async (device) => {
     const d = getKBD(device)
     if (d && d.path) {
         const id = deviceId(device)
         if(!kbd[id]){
-            kbd[id] = new HID.HID(d.path)
+            kbd[id] = await HID.HIDAsync.open(d.path);
             kbd[id].on('error', (err) => {
                 console.log(err)
                 stop(device)
